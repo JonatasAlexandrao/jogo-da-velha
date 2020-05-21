e345b15e2999c7cefa1e41fcd270ee499eaae7b0
Jonatas Alexandrao <jonatas-flash@hotmail.com>
2020-05-21 15:53:58 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite <code>src/App.js</code> e salve para atualizar a tela.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Olá Mundo do React
         </a>
       </header>
     </div>

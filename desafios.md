8d38a5220db3b9bcda4bb1d2ac12ccf5efc0a10a
Jonatas Alexandrao <jonatas-flash@hotmail.com>
2020-05-22 17:23:46 -0300

    teminado aula 48

---
+++ desafios.md
@@ -0,0 +1,4 @@
+Desafio 48
+1. criar a pasta generic e fazer ela funcionar
+2. criar a pasta elements, olhar as telas e ver quais tags deveram ser usadas varias vezes com um mesmo padrão e implementar
+3. criar a pasta settings e descobrir como criar variaveis globais no css
--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para atualizar a tela. Teste.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Olá Mundo do React
-        </a>
-      </header>
-    </div>
+
+        
+        <a href="" className="action">Sobre</a>
+
   );
 }
 

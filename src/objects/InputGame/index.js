0e2d29c08d47fb101a5ab85913155be608082b6b
Jonatas Alexandrao <jonatas-flash@hotmail.com>
2020-06-02 17:10:58 -0300

    refacturing InputGame class css

--- src/objects/InputGame/index.js
+++ src/objects/InputGame/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const InputGame = ({ id="", value="", type = "text"}) => {
   return(
-      <input className="input" type={type} value={value} id={id} />
+      <input className="input-game" type={type} value={value} id={id} />
   );
 }
 
